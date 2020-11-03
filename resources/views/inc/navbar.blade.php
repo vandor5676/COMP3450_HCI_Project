a589375de953e50036f9706e7c1b45a086a73bc0
Shane <steiner5676@gmial.com>
2020-11-02 21:07:43 -0800

    Trying to make navbar responsive

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -5,6 +5,7 @@
 <head>
   <meta charset="utf-8">
   <script src="https://kit.fontawesome.com/f85ff1d3ed.js" crossorigin="anonymous"></script>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
   <!-- <link rel="stylesheet" href="css/style.css"> -->
   <link rel="stylesheet" href="css/navBar.css">
@@ -31,26 +32,28 @@
     <!-- logo -->
     <div class="navbar__logo-container">
       <div class="logo">
-
+        <img class="navbarLogo" src={{asset("/images/logo.png")}} alt="logo">
       </div>
     </div>
     <!-- second set of items -->
     <div class="navbarItems">
-   
-    <div class="navbarItem">
+
+      <div class="navbarItem">
         <a>ACCOUNT</a>
       </div>
-      
+
       <div class="verticalCenterContainer">
         <div class="devider"></div>
       </div>
 
       <div class="navbarItem">
         <a style="color: white;">
-        <i class="fas fa-shopping-cart"></i>
-</a>
+          <i class="fas fa-shopping-cart"></i>
+        </a>
       </div>
-      
+    </div>
+  </div>
+
 
 </body>
 
