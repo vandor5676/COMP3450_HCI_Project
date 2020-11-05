80a518257651e0fd4d1f56239c703b9c753d19d6
Shane <steiner5676@gmial.com>
2020-11-05 12:46:45 -0800

    fixed responsive issue

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -53,50 +53,50 @@
         </div>
         <!-- cart item -->
         <div class="content" id="itemsContainer">
-        <div class="cartItem">
-            <div class="imageTextContainer">
-                <div class="image" id="image" style="display: inline-block;">
-                    <img class="cartItemImage" src={{asset("/images/logo.png")}} alt="logo">
-                </div>
-                <div class="text" id="text">
-                    <div>
-                        <p id="title">Farm Eggs</p>
-                        <p id="subtitle">Thistle Farm</p>
-                        <p id="stock">In Stock</p>
+            <div class="cartItem">
+                <div class="imageTextContainer">
+                    <div class="image" id="image" style="display: inline-block;">
+                        <img class="cartItemImage" src={{asset("/images/logo.png")}} alt="logo">
                     </div>
-                </div>
-            </div>
-
-            <div class="quantity" id="quantity">
-                <div class="quantityFrame">
-                    <div class="dropdown">
-                        <button onclick="myFunction()" class="dropbtn">Quantity</button>
-                        <div id="myDropdown" class="dropdown-content">
-                            <a href="">1</a>
-                            <a href="">2</a>
-                            <a href="">3</a>
+                    <div class="text" id="text">
+                        <div>
+                            <p id="title">Farm Eggs</p>
+                            <p id="subtitle">Thistle Farm</p>
+                            <p id="stock">In Stock</p>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div class="priceRemove" id="priceRemove">
-                <div class="price">
-                    <p>$2.99</p>
-                </div>
-                <div class="remove">
-                    <i class="fa fa-trash" aria-hidden="true"></i>Remove
-                </div>
-            </div>
-        </div>
 
+                <div class="quantity" id="quantity">
+                    <div class="quantityFrame">
+                        <div class="dropdown">
+                            <button onclick="myFunction()" class="dropbtn">Quantity</button>
+                            <div id="myDropdown" class="dropdown-content">
+                                <a href="">1</a>
+                                <a href="">2</a>
+                                <a href="">3</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="priceRemove" id="priceRemove">
+                    <div class="price">
+                        <p>$2.99</p>
+                    </div>
+                    <div class="remove">
+                        <i class="fa fa-trash" aria-hidden="true"></i>Remove
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <!-- end of car items -->
     </div>
+    <!-- total -->
     <div class="content" id="totalcontainer">
         <!-- <div class="cartItem"></div> -->
 
     </div>
-
-
-</div>
 
 </div>
 
