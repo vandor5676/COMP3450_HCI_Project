6bae7cfe5d7624b49e2e79c459ad56d71d947f68
Shane <steiner5676@gmial.com>
2020-11-09 13:04:16 -0800

    updates navbar links

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -32,7 +32,7 @@
     <!-- logo -->
     <div class="navbar__logo-container">
       <div class="logo">
-        <img class="navbarLogo" src={{asset("/images/logo.png")}} alt="logo">
+      <a href="/"><img class="navbarLogo" src={{asset("/images/logo.png")}} alt="logo"></a>
       </div>
     </div>
     <!-- second set of items -->
@@ -48,7 +48,7 @@
 
       <div class="navbarItem">
         <a style="color: white;">
-          <i class="fas fa-shopping-cart"></i>
+          <a href="cart"><i class="fas fa-shopping-cart"></i></a>
         </a>
       </div>
     </div>
